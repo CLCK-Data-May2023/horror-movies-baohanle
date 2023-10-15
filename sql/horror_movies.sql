64320cebcf79926c9e805346d8afbd436b00b7f4
baohanle <135090141+baohanle@users.noreply.github.com>
2023-10-15 15:50:49 +0000

    done

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,2 +1,9 @@
 -- Add your SQL here
-select * from movies
+SELECT 
+    id as Movie_ID, 
+    name as Movie_Title, 
+    imdb_rating as Rating 
+FROM movies
+WHERE year <= 1985 AND genre == 'horror'
+ORDER BY imdb_rating DESC
+LIMIT 3
